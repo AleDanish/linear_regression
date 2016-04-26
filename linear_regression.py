19f336d5ca6964cc7aa494aa19a31d7403bbe918
Alessandro <alessandrozanni.dev@gmail.com>
2016-04-26 19:08:02 +0200

    regression

--- linear_regression.py
+++ linear_regression.py
@@ -1,26 +1,62 @@
 #!/usr/bin/env python
-
 if __name__ == '__main__' and __package__ is None:
     from os import sys, path
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
+def getResults(p_acc):
+    error = ''
+    correlation = ''
+    for i, var in  enumerate(p_acc):
+        if i == len(p_acc) - 1:
+            correlation = var
+        if i == len(p_acc) - 2:
+            error = var
+    return error, correlation
+
+import numpy as np
 from svmutil import *
-datafile = 'input_data'
-#datafile='heart_scale'
+#datafile = 'input_data'
+datafile='heart_scale'
 
 # Read data in LIBSVM format
-y, x = svm_read_problem(datafile)
-problem = svm_problem(y, x)
+y, x = svm_read_problem('../../'+datafile)
+prob  = svm_problem(y, x)
 
-##### LINEAR #####
-print "y[:5]: ", y[:5]
-print "x[:5]: ", x[:5]
+max_correlation = 0
+best_conditions = ''
+best_results = [[0 for i in range(2)] for j in range(4)] 
+for t in range(0, 4):
+    if t == 0:
+        name = 'LINEAR'
+    elif t == 1:
+        name = 'POLYNOMIAL'
+    elif t == 2:
+        name = 'GAUSSIAN'
+    elif t == 3:
+        name = 'SIGMOID'
+    file = open('output/' + name, 'w')
+    file.write('### EPSILON-SRV ' + name + ' ###\n')
+    for c in range (1, 100, 10):
+        e_arr = np.arange(0.1, 1, 0.1)
+        for e in e_arr:
+            p_arr = np.arange(0.1, 1, 0.1)
+            for p in p_arr:
+                conditions = 't=' + str(t) + ' c=' + str(c) + ' e=' + str(e) + ' p=' + str(p)
+                param = svm_parameter('-q -s 3 -t '+ str(t) +' -c '+ str(c) +' -e ' + str(e) + ' -p ' + str(p))
+                m = svm_train(prob, param)
+                p_label, p_acc, p_val = svm_predict(y, x, m)
+                error, correlation = getResults(p_acc)
+                results = 'mean squared error:' + str(error) + ' - correlation:' + str(correlation) + '\n'
+                file.write(conditions + ": " + results)
+                if (best_results[t][1] < correlation):
+                    best_results[t][0] = error
+                    best_results[t][1] = correlation
+                if (max_correlation < correlation):
+                    max_correlation = correlation
+                    best_condition = conditions
+                if error >= 1:
+                    break;
+    file.close()
 
-m = svm_train(y[:5], x[:5], '-t 0 -c 5 -h 0')
-
-
-svm_save_model(datafile + '.model', m)
-p_label, p_acc, p_val = svm_predict(y[5:], x[5:], m)
-print "p_label: ",p_label
-print "p_acc: ", p_acc
-print "p_val: ",p_val
+print "best conditions: ", best_condition
+print "max correlation: ", max_correlation
