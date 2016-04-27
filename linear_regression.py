e5e5b54ac32576b90e928891be9f088d49aa7f7f
Alessandro <alessandrozanni.dev@gmail.com>
2016-04-27 15:31:00 +0200

    d param added

--- linear_regression.py
+++ linear_regression.py
@@ -38,35 +38,38 @@ for t in range(0, 4):
     print 'Inizio training e previsione per ', name
     file = open('output/' + name, 'w')
     file.write('### EPSILON-SRV ' + name + ' ###\n')
-    e_arr = np.arange(0.1, 1, 0.1)
-    p_arr = np.arange(0.1, 1, 0.1)
+    e_arr = np.arange(0.1, 1, 0.2)
+    p_arr = np.arange(0.1, 1, 0.2)
     if t == 0:
         g_arr = [1]
         r_arr = [1]
+        d_arr = [1]
     else:
-        g_arr = np.arange(1, 10)
-        r_arr = np.arange(1, 10)
+        g_arr = np.arange(1, 10, 2)
+        r_arr = np.arange(1, 10, 2)
+        d_arr = np.arange(1, 10, 2)
     for c in range (1, 100, 10):
         for e in e_arr:
             for p in p_arr:
                 for g in g_arr:
                     for r in r_arr:
-                        conditions = 't=' + str(t) + ' c=' + str(c) + ' e=' + str(e) + ' p=' + str(p) + ' g=' + str(g) + ' r=' + str(r)
-                        param = svm_parameter('-q -s 3 -t '+ str(t) +' -c '+ str(c) +' -e ' + str(e) + ' -p ' + str(p) + ' -g ' + str(g) + ' -r '+ str(r))
-                        m = svm_train(prob, param)
-                        p_label, p_acc, p_val = svm_predict(y, x, m)
-                        error, correlation = getResults(p_acc)
-                        results = 'mean squared error:' + str(error) + ' - correlation:' + str(correlation) + '\n'
-                        file.write(conditions + ": " + results)
-                        if (float(best_results[t][2]) < correlation):
-                            best_results[t][0] = conditions
-                            best_results[t][1] = str(error)
-                            best_results[t][2] = str(correlation)
-                        if (max_correlation < correlation):
-                            max_correlation = correlation
-                            best_condition = conditions
-                        if error >= 1:
-                            break;
+                        for d in d_arr:
+                            conditions = 't=' + str(t) + ' c=' + str(c) + ' e=' + str(e) + ' p=' + str(p) + ' g=' + str(g) + ' r=' + str(r)
+                            param = svm_parameter('-q -s 3 -t '+ str(t) +' -c '+ str(c) +' -e ' + str(e) + ' -p ' + str(p) + ' -g ' + str(g) + ' -r '+ str(r))
+                            m = svm_train(prob, param)
+                            p_label, p_acc, p_val = svm_predict(y, x, m)
+                            error, correlation = getResults(p_acc)
+                            results = 'mean squared error:' + str(error) + ' - correlation:' + str(correlation) + '\n'
+                            file.write(conditions + ": " + results)
+                            if (float(best_results[t][2]) < correlation):
+                                best_results[t][0] = conditions
+                                best_results[t][1] = str(error)
+                                best_results[t][2] = str(correlation)
+                            if (max_correlation < correlation):
+                                max_correlation = correlation
+                                best_condition = conditions
+                            if error >= 1:
+                                break;
     print 'Finiti training e previsione per ', name
     file.close()
 
