7a946187a2a3f11ec3ceb02f7f8614b451923bbf
Alessandro <alessandrozanni.dev@gmail.com>
2016-04-27 15:24:33 +0200

    parameters added

--- linear_regression.py
+++ linear_regression.py
@@ -13,6 +13,16 @@ def getResults(p_acc):
             error = var
     return error, correlation
 
+def getFunction(t):
+    if t == 0:
+        return 'LINEAR'
+    elif t == 1:
+        return 'POLYNOMIAL'
+    elif t == 2:
+        return 'GAUSSIAN'
+    elif t == 3:
+        return 'SIGMOID'
+
 import numpy as np
 from svmutil import *
 # Read data in LIBSVM format
@@ -24,50 +34,49 @@ best_conditions = ''
 best_results = [['0' for i in range(3)] for j in range(4)]
 
 for t in range(0, 4):
-    if t == 0:
-        name = 'LINEAR'
-    elif t == 1:
-        name = 'POLYNOMIAL'
-    elif t == 2:
-        name = 'GAUSSIAN'
-    elif t == 3:
-        name = 'SIGMOID'
+    name = getFunction(t)
+    print 'Inizio training e previsione per ', name
     file = open('output/' + name, 'w')
     file.write('### EPSILON-SRV ' + name + ' ###\n')
+    e_arr = np.arange(0.1, 1, 0.1)
+    p_arr = np.arange(0.1, 1, 0.1)
+    if t == 0:
+        g_arr = [1]
+        r_arr = [1]
+    else:
+        g_arr = np.arange(1, 10)
+        r_arr = np.arange(1, 10)
     for c in range (1, 100, 10):
-        e_arr = np.arange(0.1, 1, 0.1)
         for e in e_arr:
-            p_arr = np.arange(0.1, 1, 0.1)
             for p in p_arr:
-                conditions = 't=' + str(t) + ' c=' + str(c) + ' e=' + str(e) + ' p=' + str(p)
-                param = svm_parameter('-q -s 3 -t '+ str(t) +' -c '+ str(c) +' -e ' + str(e) + ' -p ' + str(p))
-                m = svm_train(prob, param)
-                p_label, p_acc, p_val = svm_predict(y, x, m)
-                error, correlation = getResults(p_acc)
-                results = 'mean squared error:' + str(error) + ' - correlation:' + str(correlation) + '\n'
-                file.write(conditions + ": " + results)
-                if (float(best_results[t][2]) < correlation):
-                    best_results[t][0] = conditions
-                    best_results[t][1] = str(error)
-                    best_results[t][2] = str(correlation)
-                if (max_correlation < correlation):
-                    max_correlation = correlation
-                    best_condition = conditions
-                if error >= 1:
-                    break;
+                for g in g_arr:
+                    for r in r_arr:
+                        conditions = 't=' + str(t) + ' c=' + str(c) + ' e=' + str(e) + ' p=' + str(p) + ' g=' + str(g) + ' r=' + str(r)
+                        param = svm_parameter('-q -s 3 -t '+ str(t) +' -c '+ str(c) +' -e ' + str(e) + ' -p ' + str(p) + ' -g ' + str(g) + ' -r '+ str(r))
+                        m = svm_train(prob, param)
+                        p_label, p_acc, p_val = svm_predict(y, x, m)
+                        error, correlation = getResults(p_acc)
+                        results = 'mean squared error:' + str(error) + ' - correlation:' + str(correlation) + '\n'
+                        file.write(conditions + ": " + results)
+                        if (float(best_results[t][2]) < correlation):
+                            best_results[t][0] = conditions
+                            best_results[t][1] = str(error)
+                            best_results[t][2] = str(correlation)
+                        if (max_correlation < correlation):
+                            max_correlation = correlation
+                            best_condition = conditions
+                        if error >= 1:
+                            break;
+    print 'Finiti training e previsione per ', name
     file.close()
 
+print "Standard Sigmoid:"
+param = svm_parameter('-s 3 -t 3')
+m = svm_train(prob, param)
+p_label, p_acc, p_val = svm_predict(y, x, m)
+
 for t in range(0, 4):
-    function = ''
-    if t == 0:
-        function = 'LINEAR'
-    elif t == 1:
-       function = 'POLYNOMIAL'
-    elif t == 2:
-        function = 'GAUSSIAN'
-    elif t == 3:
-        function = 'SIGMOID'
-    print function + ' - best prediction'
+    print getFunction(t) + ' - best prediction'
     print 'parameters: ', best_results[t][0]
     print 'error: ', best_results[t][1]
     print 'correlation: ', best_results[t][2]
